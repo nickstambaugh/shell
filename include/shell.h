4cae980cb709cd223f060082c7c72ba95ef231c1
Nick Stambaugh <nastambaugh@gmail.com>
2024-03-05 22:21:20 -0500

    cmd header

---
+++ include/shell.h
@@ -0,0 +1,11 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include "IO.h"
+
+void shell_loop(void);
+
+void shell_loop_test(void);
+
+void shell_stop(void);
+#endif /* SHELL_H */
--- src/main.c
+++ src/main.c
@@ -1,8 +1,31 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "custom_string.h"
+#include "../include/IO.h"
+#include "../include/custom_string.h"
+#include "../include/shell.h"
+#include "../include/creds.h"
+#include "../include/utils.h"
+#include <time.h>
 
-int main(void) {
-    printf("hello world!");
+
+void test_shell_loop(void)
+{
+    clock_t start = clock();
+    char mode[2] = "r";
+    char path[49] = "C:\\Users\\Stambaugh\\learn-c\\src\\Test.txt";
+    FILE *f = FOPEN(path, mode);
+    set_input_file(f);
+    shell_loop_test();
+    set_input_file(stdin);
+    FCLOSE(f);
+    printf("\nTest File Runtime: %lums\n",clock()-start);
+}
+
+int main(void)
+{
+    /*
+    for (int i = 0; i < 10; i++)
+        test_creds();
+    */
+    test_shell_loop();
+    shell_loop();
     return 0;
 }
